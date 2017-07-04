f5993ef796224fb88372ae7ef0d1f46f0860a999
Pincho <pinchodev91@gmail.com>
2017-07-04 10:56:48 +0200

    Add clases

---
+++ MaquinaEstadosUnity Project/Assets/Maquina Estados Unity Proyectos/Scripts/Clases/State.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clase Abstracta : Solo con metodos abstractos, obliga a tener los metodos en todas las
+// clases que hereden de state
+public abstract class State
+{
+	public abstract void Execute ();
+}
+
+public class EnterMine : State
+{
+	public override void Execute()
+	{
+		Debug.Log("Mine");
+	}
+}
+
+public class VisitBank : State
+{
+	public override void Execute()
+	{
+		Debug.Log("Bank");
+	}
+}
+
+public class Quench : State
+{
+	public override void Execute()
+	{
+		Debug.Log("Quench");
+	}
+}
+
+public class GoHome : State
+{
+	public override void Execute()
+	{
+		Debug.Log("Home");
+	}
+}
---
+++ MaquinaEstadosUnity Project/Assets/Maquina Estados Unity Proyectos/Scripts/Sistemas/FSMController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMController : MonoBehaviour {
+
+	State currentState;
+
+	void Start()
+	{
+		currentState = new EnterMine();
+	}
+
+	void Update()
+	{
+		currentState.Execute();
+	}
+}
