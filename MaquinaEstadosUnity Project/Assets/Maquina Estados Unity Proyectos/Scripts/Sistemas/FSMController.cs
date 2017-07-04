5295546899784be2682e9b26010b17f12ad578a8
Pincho <pinchodev91@gmail.com>
2017-07-04 11:06:57 +0200

    Implementado Cambio de estado

--- MaquinaEstadosUnity Project/Assets/Maquina Estados Unity Proyectos/Scripts/Sistemas/FSMController.cs
+++ MaquinaEstadosUnity Project/Assets/Maquina Estados Unity Proyectos/Scripts/Sistemas/FSMController.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MinerController))]
 public class FSMController : MonoBehaviour {
 
+	State prevState;
 	State currentState;
+	public MinerController player;
 
 
 	void Start()
 	{
+		player = this.GetComponent<MinerController>();
 		currentState = new EnterMine();
 	}
 
 	void Update()
 	{
 		currentState.Execute();
+	}
+
+	public void ChangeState(State newState)
+	{
+		prevState = currentState;
+		currentState = newState;
 	}
 }
