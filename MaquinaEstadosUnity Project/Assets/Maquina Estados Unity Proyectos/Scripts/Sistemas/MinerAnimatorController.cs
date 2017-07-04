293926b838839a8bd8f180f55a3b7d98f94c6abf
Pincho <pinchodev91@gmail.com>
2017-07-04 12:05:13 +0200

    Implementado funcionalidad parcial con Animator

---
+++ MaquinaEstadosUnity Project/Assets/Maquina Estados Unity Proyectos/Scripts/Sistemas/MinerAnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerAnimatorController : MonoBehaviour {
+
+	MinerController player;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		player = this.GetComponent<MinerController>();
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetInteger("Oro", player.oro);
+		anim.SetInteger("Cansancio", player.cansancio);
+		anim.SetInteger("Dinero", player.monedas);
+		anim.SetInteger("Sed", player.sed);
+	}
+}
